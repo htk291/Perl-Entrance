de586c5a7d26d62a9523d4e03460602a0e50b287
htk291 <htk0129@gmail.com>
2014-06-28 13:53:43 +0900

    add profile.pl

---
+++ 02_2014-06-28/02_profile.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $name = 'htk291';
+my $age  = 27;
+my $height = 176.5;
+my $weight = 68;
+
+print "NAME : $name, AGE : $age, HEIGHT : $height, WEIGHT : ${weight}kg\n";
