46b1db84c01183b8e568d38ceb9024c936602792
htk291 <htk291@gmail.com>
2015-02-21 15:11:34 +0900

    add while.pl

---
+++ 05_2015-02-21/04_while.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+&main() unless caller;
+
+sub main {
+    while (1) {
+        print '>> ';
+        chomp( my $foo = <STDIN> );
+        last unless $foo;
+
+        &calc_string($foo);
+    }
+}
+
+sub calc_string {
+    my $arg = shift;
+    print "ERROR!!\n" unless $arg =~ /^(\d+) ([+\-*\/\%]) (\d+)$/;
+
+    print ( $1 + $3."\n" ) if $2 eq '+';
+    print ( $1 - $3."\n" ) if $2 eq '-';
+    print ( $1 * $3."\n" ) if $2 eq '*';
+    print ( $1 / $3."\n" ) if $2 eq '/';
+    print ( $1 % $3."\n" ) if $2 eq '%';
+}
+
+__DATA__
+my ($x, $op, $y) = $arg =~ /^(\d+) ([+\-*\/\%]) (\d+)$/;
+
+print ( $x + $y."\n" ) if $op eq '+';
+print ( $x - $y."\n" ) if $op eq '-';
+print ( $x * $y."\n" ) if $op eq '*';
+print ( $x / $y."\n" ) if $op eq '/';
+print ( $x % $y."\n" ) if $op eq '%';
